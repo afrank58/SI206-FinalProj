650867743968e012e14cc98edb9090ae6c14cc0c
Alexandra Lepore <alexandralepore@Alexandras-MacBook-Pro.local>
2021-04-21 13:45:13 -0400

    added more tables

--- WebScrape.py
+++ WebScrape.py
@@ -26,15 +26,45 @@ def get_player_towns_states(tup_lst):
 
     return final_tup_lst 
 
+def get_players_in_state(tup_list):
+    in_state_players = []
+    for tup in tup_list:
+        if tup[3] == "Mich.":
+            in_state_players.append(tup)
+    return in_state_players 
+
+def get_basketball_players_in_state(in_state_tup_list):
+    in_state_basketball_players = []
+    for tup in in_state_tup_list:
+        if tup[0] == "Basketball":
+            in_state_basketball_players.append(tup)
+    return in_state_basketball_players
 
 
-#def get_per_in_state(tup_lst):
-    #states = []
-    #for tup in tup_lst:
-        #states.append(tup[3])
-    #num_in_state = states.count('Mich.')
-    #per_in_state = num_in_state / len(tup_lst)
-    #return per_in_state
+def get_football_players_in_state(in_state_tup_list):
+    in_state_football_players = []
+    for tup in in_state_tup_list:
+        if tup[0] == "Football":
+            in_state_football_players.append(tup)
+    return in_state_football_players
+
+
+def get_hockey_players_in_state(in_state_tup_list):
+    in_state_hockey_players = []
+    for tup in in_state_tup_list:
+        if tup[0] == "Hockey":
+            in_state_hockey_players.append(tup)
+    return in_state_hockey_players
+
+
+
+def get_per_in_state(tup_lst):
+    states = []
+    for tup in tup_lst:
+        states.append(tup[3])
+    num_in_state = states.count('Mich.')
+    per_in_state = num_in_state / len(tup_lst)
+    return per_in_state
 
 
 
@@ -64,17 +94,49 @@ def setUpPlayersTable(tup_lst, cur, conn):
     
     conn.commit()
 
-def get_per_in_state(cur, conn):
-    states_list = []
-    cur.execute('SELECT homestate FROM Players')
-    states = cur.fetchall()
-    for state in states:
-        states_list.append(state)
 
-    num_in_state = states_list.count('Mich.')
-    per_in_state = num_in_state / len(states_list)
+def setUpInStateBaskbetballTable(in_state_basketball_players, cur, conn):
+    cur.execute('CREATE TABLE IF NOT EXISTS Basketball_In_State (sport_id TEXT, name TEXT, hometown TEXT, homestate TEXT)')
+    for tup in in_state_basketball_players:
+        cur.execute('SELECT sport_id FROM Sports WHERE sport_name =?', (tup[0],))
+        sport_id = cur.fetchone()[0]
+        cur.execute('INSERT INTO Basketball_In_State (sport_id, name, hometown, homestate) VALUES (?, ?, ?, ?)', (sport_id, tup[1], tup[2], tup[3]))
+    
+    conn.commit()
 
-    return per_in_state
+
+def setUpInStateFootballTable(in_state_football_players, cur, conn):
+    cur.execute('CREATE TABLE IF NOT EXISTS Football_In_State (sport_id TEXT, name TEXT, hometown TEXT, homestate TEXT)')
+    for tup in in_state_football_players:
+        cur.execute('SELECT sport_id FROM Sports WHERE sport_name =?', (tup[0],))
+        sport_id = cur.fetchone()[0]
+        cur.execute('INSERT INTO Football_In_State (sport_id, name, hometown, homestate) VALUES (?, ?, ?, ?)', (sport_id, tup[1], tup[2], tup[3]))
+    
+    conn.commit()
+
+
+
+def setUpInStateHockeyTable(in_state_hockey_players, cur, conn):
+    cur.execute('CREATE TABLE IF NOT EXISTS Hockey_In_State (sport_id TEXT, name TEXT, hometown TEXT, homestate TEXT)')
+    for tup in in_state_hockey_players:
+        cur.execute('SELECT sport_id FROM Sports WHERE sport_name =?', (tup[0],))
+        sport_id = cur.fetchone()[0]
+        cur.execute('INSERT INTO Hockey_In_State (sport_id, name, hometown, homestate) VALUES (?, ?, ?, ?)', (sport_id, tup[1], tup[2], tup[3]))
+    
+    conn.commit()
+
+
+#def get_per_in_state(cur, conn):
+    #states_list = []
+    #cur.execute('SELECT homestate FROM Players')
+    #states = cur.fetchall()
+    #for state in states:
+    #    states_list.append(state)
+
+    #num_in_state = states_list.count('Mich.')
+    #per_in_state = num_in_state / len(states_list)
+
+    #return per_in_state
 
 
 
@@ -83,10 +145,10 @@ def write_data_to_file(filename, cur, conn):
     path = os.path.dirname(os.path.abspath(__file__)) + os.sep
     outFile = open(path + filename, "w")
 
-    #percentage = get_per_in_state(get_player_towns_states([('Basketball', 'https://mgoblue.com/sports/mens-basketball/roster'), ('Football', 'https://mgoblue.com/sports/football/roster'), ('Hockey', 'https://mgoblue.com/sports/mens-ice-hockey/roster')]))
-    percentage = get_per_in_state(cur, conn)
+    percentage = get_per_in_state(get_player_towns_states([('Basketball', 'https://mgoblue.com/sports/mens-basketball/roster'), ('Football', 'https://mgoblue.com/sports/football/roster'), ('Hockey', 'https://mgoblue.com/sports/mens-ice-hockey/roster')]))
+    #percentage = get_per_in_state(cur, conn)
     outFile.write("Percentage of athletes that are from Michigan on the Men's Basketball, Football, and Hockey Teams. \n")
-    outFile.write(str(percentage) + " of Michigan Men's Backetball, Football, and Hockey players are from the state of Michigan. \n")
+    outFile.write(str(percentage) + " of Michigan Men's Basketball, Football, and Hockey players are from the state of Michigan. \n")
     outFile.close()
 
 
@@ -95,15 +157,24 @@ def main():
     list_tups = [('Basketball', 'https://mgoblue.com/sports/mens-basketball/roster'), ('Football', 'https://mgoblue.com/sports/football/roster'), ('Hockey', 'https://mgoblue.com/sports/mens-ice-hockey/roster')]
     print(get_player_towns_states([('Basketball', 'https://mgoblue.com/sports/mens-basketball/roster'), ('Football', 'https://mgoblue.com/sports/football/roster'), ('Hockey', 'https://mgoblue.com/sports/mens-ice-hockey/roster')]))
     print(len(get_player_towns_states([('Basketball', 'https://mgoblue.com/sports/mens-basketball/roster'), ('Football', 'https://mgoblue.com/sports/football/roster'), ('Hockey', 'https://mgoblue.com/sports/mens-ice-hockey/roster')])))
-    #print(get_per_in_state(get_player_towns_states([('Basketball', 'https://mgoblue.com/sports/mens-basketball/roster'), ('Football', 'https://mgoblue.com/sports/football/roster'), ('Hockey', 'https://mgoblue.com/sports/mens-ice-hockey/roster')])))
+    print(get_players_in_state(get_player_towns_states(list_tups)))
+    print(get_basketball_players_in_state(get_players_in_state(get_player_towns_states(list_tups))))
+    print(get_football_players_in_state(get_players_in_state(get_player_towns_states(list_tups))))
+    print(get_hockey_players_in_state(get_players_in_state(get_player_towns_states(list_tups))))
+    print(get_per_in_state(get_player_towns_states([('Basketball', 'https://mgoblue.com/sports/mens-basketball/roster'), ('Football', 'https://mgoblue.com/sports/football/roster'), ('Hockey', 'https://mgoblue.com/sports/mens-ice-hockey/roster')])))
 
 
     cur, conn = setUpDatabase('players.db')
     setUpSportsTable(cur, conn)
 
     setUpPlayersTable(get_player_towns_states(list_tups), cur, conn)
+
+    setUpInStateBaskbetballTable(get_basketball_players_in_state(get_players_in_state(get_player_towns_states(list_tups))), cur, conn)
+    setUpInStateFootballTable(get_football_players_in_state(get_players_in_state(get_player_towns_states(list_tups))), cur, conn)
+    setUpInStateHockeyTable(get_hockey_players_in_state(get_players_in_state(get_player_towns_states(list_tups))), cur, conn)
+
     write_data_to_file("Players.txt", cur, conn)
-    print(get_per_in_state(cur, conn))
+    #print(get_per_in_state(cur, conn))
     
     conn.close()
 
@@ -111,11 +182,12 @@ def main():
     labels = ['In State Players', 'Out of State Players']
     sizes = [32.28, 67.72]
     colors = ['blue', 'yellow']
-    #explode = (0.1, 0, 0, 0)  # explode 1st slice
+    explode = (0.1, 0)  # explode 1st slice
     
     # Plot
-    plt.pie(sizes, labels=labels, colors=colors,
-            autopct='%1.1f%%', startangle=140)
+    plt.pie(sizes, explode = explode, labels=labels, colors=colors,
+    autopct='%1.1f%%', startangle=140)
+    plt.title("Percentage of UMich Men's Basketball, Football, and Hockey Players \n" + "that are from in state versus out of state")
     
     plt.axis('equal')
     plt.show()  
